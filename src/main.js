a9a0b6fb4dcff6446028a514b0c9cc084c983170
web1992 <554933654@qq.com>
2019-03-27 16:26:06 +0800

    fix vue

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import Vant from 'vant';
 import 'vant/lib/index.css';
 
-import VueAwesomeSwiper from 'vue-awesome-swiper'
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 // require styles
-import 'swiper/dist/css/swiper.css'
-Vue.use(VueAwesomeSwiper)
+import 'swiper/dist/css/swiper.css';
+Vue.use(VueAwesomeSwiper);
 
 Vue.use(Vant);
 Vue.config.productionTip = false
